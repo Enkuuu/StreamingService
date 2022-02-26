4d9ef3bb59481cd454386b275988adef18ae6784
Enku <econamacy@gmail.com>
2022-02-26 10:03:49 -0800

    files
    please help

---
+++ script.js
@@ -0,0 +1,63 @@
+import {Show} from './show.js'
+
+let library = [
+    new Show(
+        "Placeholder",
+        4444,
+        "Place",
+        "ahahhahaha",
+        "placeholder-image.png"
+    ),
+
+    new Show(
+        "Placeholder",
+        4444,
+        "Place",
+        "ahahhahaha",
+        "placeholder-image.png"
+    ),
+
+    new Show(
+        "Placeholder",
+        4444,
+        "Place",
+        "ahahhahaha",
+        "placeholder-image.png"
+    ),
+
+    new Show(
+        "Placeholder",
+        4444,
+        "Place",
+        "ahahhahaha",
+        "placeholder-image.png"
+    ),
+
+    new Show(
+        "Placeholder",
+        4444,
+        "Place",
+        "ahahhahaha",
+        "placeholder-image.png"
+    ),
+
+    new Show(
+        "Placeholder",
+        4444,
+        "Place",
+        "ahahhahaha",
+        "placeholder-image.png"
+    ),
+
+    new Show(
+        "Placeholder",
+        4444,
+        "Place",
+        "ahahhahaha",
+        "placeholder-image.png"
+    ),
+]
+
+library.forEach(function(show){
+    show.addToPage()
+})
---
+++ show.js
@@ -0,0 +1,39 @@
+class Show{
+    constructor(name, year, genre, description, image){
+        this.name = name;
+        this.year = year;
+        this.genre = genre;
+        this.description = description;
+        this.image = image;
+    }
+
+    addToPage(){
+        if(document.getElementById(this.genre)){
+            document.getElementById(this.genre).append(this.buildShow);
+        }
+        else{
+            this.createGenre();
+        }
+    }
+
+    buildShow(){
+        let showEl = document.createElement('div');
+        showEl.classList.add('show');
+        showEl.style.backgroundImage = 'url('+this.image + ')';
+        return showEl;
+    }
+
+    createGenre(){
+        let genre = document.createElement('div');
+        genre.classList.add('genre');
+        let title = document.createElement('h2');
+        title.innerHTML = this.genre;
+        genre.id = this.genre;
+        document.getElementById('library').append(title);
+        document.getElementById('library').append(genre);
+
+        this.addToPage();
+    }
+}
+
+export{Show}
